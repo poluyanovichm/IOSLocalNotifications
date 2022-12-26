a75d27902ff50436c640cb4811dfd1e852f111a9
mpoluyanovich <poluyanovich@mail.ru>
2022-12-26 12:00:47 +0300

    Initial Commit

---
+++ IOSLocalNotifications/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOSLocalNotifications
+//
+//  Created by Mikhail on 26.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
